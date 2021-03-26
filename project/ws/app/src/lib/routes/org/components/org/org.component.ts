0025def977734576e97b8b1bb0caa7899c7141aa
revathip <revathipp@gmail.com>
2021-03-26 17:10:11 +0530

    org profile feature completd

---
+++ project/ws/app/src/lib/routes/org/components/org/org.component.ts
@@ -0,0 +1,118 @@
+import { OrgServiceService } from './../../org-service.service'
+import { Component, OnInit } from '@angular/core'
+import { ActivatedRoute, Router, Data } from '@angular/router'
+
+@Component({
+  selector: 'ws-app-org',
+  templateUrl: './org.component.html',
+  styleUrls: ['./org.component.scss']
+})
+export class OrgComponent implements OnInit {
+  orgName: any
+  courseData!: Object
+  routeSubscription: any
+  orgData: any
+  currentOrgData: any
+
+  constructor(private activateRoute: ActivatedRoute, private orgService: OrgServiceService,
+    private router: Router) { }
+
+  ngOnInit() {
+    this.orgName = this.activateRoute.snapshot.queryParams.orgId
+    this.routeSubscription = this.activateRoute.data.subscribe((response: Data) => {
+      const currentOrg = this.orgName
+      if (response.orgData) {
+        this.orgData = response.orgData.data.sources
+        this.currentOrgData = this.orgData.filter(
+          (org: any) => org.sourceName === currentOrg
+        )
+        if (this.currentOrgData) {
+          this.currentOrgData = this.currentOrgData[0]
+        }
+      }
+    })
+    const req = {
+      "orgId": [this.orgName],
+      "searchFilters": {
+        "locale": [
+          "en"
+        ],
+        "pageSize": 5,
+        "query": "all",
+        "didYouMean": true,
+        "filters": [
+          {
+            "andFilters": [
+              {
+                "contentType": [
+                  "Course",
+                  "Program"
+                ]
+              }
+            ]
+          }
+        ],
+        "visibleFilters": {
+          "learningMode": {
+            "displayName": "Mode"
+          },
+          "duration": {
+            "displayName": "Duration"
+          },
+          "exclusiveContent": {
+            "displayName": "Costs"
+          },
+          "complexityLevel": {
+            "displayName": "Level"
+          },
+          "catalogPaths": {
+            "displayName": "Catalog",
+            "order": [
+              {
+                "_key": "asc"
+              }
+            ]
+          },
+          "sourceShortName": {
+            "displayName": "Source"
+          },
+          "resourceType": {
+            "displayName": "Format"
+          },
+          "region": {
+            "displayName": "Region"
+          },
+          "concepts": {
+            "displayName": "Concepts"
+          },
+          "lastUpdatedOn": {
+            "displayName": "Published Date"
+          }
+        },
+        "includeSourceFields": [
+          "creatorLogo"
+        ],
+        "sort": [
+          {
+            "lastUpdatedOn": "desc"
+          }
+        ]
+      }
+    }
+    this.orgService.getDatabyOrgId(req).subscribe(data => {
+      this.courseData = data
+    })
+  }
+
+  gotoOverview(identifier: any) {
+    this.router.navigate([`/app/toc/${identifier}/overview`])
+  }
+
+  showMoreCourses() {
+    this.router.navigate(['/app/org-details/all-courses'], { queryParams: { orgId: this.orgName } })
+  }
+
+  goToProfile(id: string) {
+    this.router.navigate(['/app/person-profile'], { queryParams: { userId: id } })
+  }
+}
