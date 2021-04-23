e3e2a3a0277535e6256ed07a838893d8e9c67618
revathip <revathipp@gmail.com>
2021-04-23 20:02:56 +0530

    SCI:65, SCI-70: Content not saving fix, multiple web page on learner flow enabled

--- project/ws/app/src/lib/routes/org/components/org/org.component.ts
+++ project/ws/app/src/lib/routes/org/components/org/org.component.ts
@@ -1,7 +1,8 @@
 import { AuthKeycloakService } from '@ws-widget/utils'
 import { OrgServiceService } from './../../org-service.service'
-import { Component, OnInit } from '@angular/core'
+import { Component, OnInit, ViewChild } from '@angular/core'
 import { ActivatedRoute, Router, Data } from '@angular/router'
+import { MdePopoverTrigger } from '@material-extended/mde'
 
 @Component({
   selector: 'ws-app-org',
@@ -9,14 +10,16 @@ import { ActivatedRoute, Router, Data } from '@angular/router'
   styleUrls: ['./org.component.scss'],
 })
 export class OrgComponent implements OnInit {
+  @ViewChild('target', { static: false }) target!: MdePopoverTrigger
   orgName: any
   courseData!: any
   routeSubscription: any
   orgData: any
   currentOrgData: any
+  showEndPopup: boolean = false
 
   constructor(private activateRoute: ActivatedRoute, private orgService: OrgServiceService,
-              private router: Router, private authSvc: AuthKeycloakService) { }
+    private router: Router, private authSvc: AuthKeycloakService) { }
 
   ngOnInit() {
     this.orgName = this.activateRoute.snapshot.queryParams.orgId
@@ -109,7 +112,7 @@ export class OrgComponent implements OnInit {
     if (this.authSvc.isAuthenticated) {
       this.router.navigate([`/app/toc/${identifier}/overview`])
     } else {
-      this.authSvc.login('S', document.URL)
+      this.authSvc.login('S', document.baseURI + `/app/toc/${identifier}/overview`)
     }
   }
 
@@ -119,5 +122,17 @@ export class OrgComponent implements OnInit {
 
   goToProfile(id: string) {
     this.router.navigate(['/app/person-profile'], { queryParams: { userId: id } })
+  }
+  showTarget(event: any) {
+    if (window.innerWidth - event.clientX < 483) {
+      this.showEndPopup = true
+      this.target.targetOffsetX = event.clientX + 1
+    } else {
+      // console.log('this.showEndPopup', this.showEndPopup)
+    }
+  }
+  loginRedirect(key: 'E' | 'N' | 'S', contentId: any) {
+    const url = `/app/toc/${contentId}/overview`
+    this.authSvc.login(key, url)
   }
 }
