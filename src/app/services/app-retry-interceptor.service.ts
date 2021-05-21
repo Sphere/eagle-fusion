81ad0f5f4a50a3dca47a74b3352148e381b3c601
revathip <revathipp@gmail.com>
2021-05-21 12:55:52 +0530

    inc responsive bug fix, retry stopped for verifywithmobile api

--- src/app/services/app-retry-interceptor.service.ts
+++ src/app/services/app-retry-interceptor.service.ts
@@ -20,10 +20,10 @@ export class AppRetryInterceptorService implements HttpInterceptor {
       return next.handle(req)
     }
     if (req.url.indexOf('registerUserWithMobile') > 0 || req.url.indexOf('setPasswordWithOTP') > 0 ||
-      req.url.indexOf('registerUserWithEmail') > 0) {
+      req.url.indexOf('registerUserWithEmail') > 0 || req.url.indexOf('verifyUserWithMobileNumber') > 0) {
       return next.handle(req).pipe(retryWhen(this.genericRetryStrategyVerifyMobile()))
     }
-      return next.handle(req).pipe(retryWhen(this.genericRetryStrategy()))
+    return next.handle(req).pipe(retryWhen(this.genericRetryStrategy()))
 
   }
 
@@ -45,15 +45,8 @@ export class AppRetryInterceptorService implements HttpInterceptor {
 
   private genericRetryStrategyVerifyMobile = () => (attempts: Observable<any>) =>
     attempts.pipe(
-      mergeMap((error: HttpErrorResponse, i: number) => {
-        const retryAttempt = i + 1
-        // if maximum number of retries have been met
-        // or response is a status code we don't wish to retry, throw error
-        if (!this.shouldRetry(error)) {
-          return throwError(error)
-        }
-        // retry after 1s, 2s, etc...
-        return timer(retryAttempt * this.scalingDuration)
+      mergeMap((error: HttpErrorResponse) => {
+        return throwError(error)
       }),
     )
 }
