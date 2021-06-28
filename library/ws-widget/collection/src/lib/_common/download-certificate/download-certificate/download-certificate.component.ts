0578a918a2d271f60d27038007b945c24066f050
revathip <revathipp@gmail.com>
2021-06-28 19:24:05 +0530

    Resolved conflicts in development branch

--- library/ws-widget/collection/src/lib/_common/download-certificate/download-certificate/download-certificate.component.ts
+++ library/ws-widget/collection/src/lib/_common/download-certificate/download-certificate/download-certificate.component.ts
@@ -86,8 +86,9 @@ export class DownloadCertificateComponent implements OnInit {
             const dialogRef = this.dialog.open(DialogConfirmComponent, {
               data: {
                 title: 'RN Number is missing from your profile',
-                body: `If you are a Registered Nurse/Midwife please update your Registration Number (RN Number) on the
-                profile and then try downloading the certificate. If you don't have RN Number click on 'Download Now'`,
+                body: `If you are a Registered Nurse/Midwife please update your Registration
+                 Number (RN Number) on the profile and then try downloading the certificate.
+                If you don't have RN Number click on 'Download Now'`,
               },
               width: '600px',
             })
