3e54fd146148108dbd95fd300f48e87b7c65598f
revathip <revathipp@gmail.com>
2021-07-09 19:30:51 +0530

    SCI-140: mobile view tab retain fix, SCI-146: Content overlap fixed

--- library/ws-widget/collection/src/lib/_common/download-certificate/download-certificate/download-certificate.component.ts
+++ library/ws-widget/collection/src/lib/_common/download-certificate/download-certificate/download-certificate.component.ts
@@ -291,7 +291,7 @@ export class DownloadCertificateComponent implements OnInit {
             }
           }
         },
-          err => {
+                                                     err => {
             this.openSnackbar(err)
           })
 
@@ -335,7 +335,7 @@ export class DownloadCertificateComponent implements OnInit {
             doc.save('certificate_pocqi.pdf')
           }
         },
-          err => {
+                                                     err => {
             this.openSnackbar(err)
           })
       }
