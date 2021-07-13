124f4ebb6b544a344c292544c2087cd99fadc1e4
Prabinaaa <prabina.parichha@tarento.com>
2021-07-13 20:03:41 +0530

    Lint fix for dropdown feature for country, state, district added

--- project/ws/app/src/lib/routes/user-profile/chatbot/chatbot/chatbot.component.ts
+++ project/ws/app/src/lib/routes/user-profile/chatbot/chatbot/chatbot.component.ts
@@ -2,7 +2,11 @@ import { Component, ElementRef, OnInit, ViewChild } from '@angular/core'
 import { HttpClient } from '@angular/common/http'
 import { FormControl, FormGroup } from '@angular/forms'
 import { UserProfileService } from '../../../user-profile/services/user-profile.service'
+import { INationality } from '../../../user-profile/models/user-profile.model'
+import { map, startWith } from 'rxjs/operators'
+import { Observable, of } from 'rxjs'
 import moment from 'moment'
+import { Router } from '@angular/router'
 declare var $: any
 @Component({
   selector: 'ws-app-chatbot',
@@ -14,6 +18,7 @@ export class ChatbotComponent implements OnInit {
   nextId: any
   _chatFormValue: any
   chatUrl = '/fusion-assets/files/Registation-chatbot-model.json'
+  districtUrl = '/fusion-assets/files/district.json'
   chatObj: any = []
   chatArray: any = []
   inputMsgEnabled = false
@@ -26,13 +31,29 @@ export class ChatbotComponent implements OnInit {
   @ViewChild('chatoutput', { static: false }) outputArea!: ElementRef
   errMsg = ''
 
+  masterNationalities: any = []
+  filteredOptions: Observable<string[]> | undefined
+  filteredOptionsState: Observable<string[]> | undefined
+  filteredOptionsDistrict: Observable<string[]> | undefined
+  states: any = []
+  statesArr: any = []
+  dropdownStatus = ''
+  district: any = []
+  districtArr: any = []
+  enableInputForDropdown = false
+
   constructor(private http: HttpClient,
-    private userProfileSvc: UserProfileService) { }
+    private userProfileSvc: UserProfileService,
+    private router: Router) { }
 
   ngOnInit() {
     this.http.get(this.chatUrl).subscribe(data => {
       this.chatObj = data
       this.chatArray.push(this.chatObj.regOption.profiledetails[0])
+    })
+
+    this.http.get(this.districtUrl).subscribe((data: any) => {
+      this.district = data.states
     })
 
     this.createChatForm = new FormGroup({
@@ -50,7 +71,137 @@ export class ChatbotComponent implements OnInit {
       organisationType: new FormControl('', []),
       designation: new FormControl('', []),
       designationOther: new FormControl('', []),
+      residenceState: new FormControl('', []),
+      residenceDistrict: new FormControl('', []),
     })
+
+    this.fetchMeta()
+
+  }
+
+  fetchMeta() {
+    this.userProfileSvc.getMasterNationlity().subscribe(
+      data => {
+        data.nationalities.map((item: INationality) => {
+          this.masterNationalities.push(item.name)
+        })
+      },
+      (_err: any) => {
+      })
+
+    this.userProfileSvc.getProfilePageMeta().subscribe(
+      data => {
+        this.states = data.states
+
+        this.states.map((i: any) => {
+          this.statesArr.push(i.name)
+        })
+      },
+      (_err: any) => {
+      })
+
+    // tslint:disable-next-line: no-non-null-assertion
+    this.filteredOptions = this.createUserForm.get('residenceAddress')!.valueChanges
+      .pipe(
+        startWith(''),
+        map(value => this.councilFilter(value))
+      )
+    // tslint:disable-next-line: no-non-null-assertion
+    this.filteredOptionsState = this.createUserForm.get('residenceState')!.valueChanges
+      .pipe(
+        startWith(''),
+        map(value => this.councilFilter(value))
+      )
+    // tslint:disable-next-line: no-non-null-assertion
+    this.filteredOptionsDistrict = this.createUserForm.get('residenceDistrict')!.valueChanges
+      .pipe(
+        startWith(''),
+        map(value => this.councilFilter(value))
+      )
+
+  }
+
+  private councilFilter(value: string): string[] {
+    const key = this.dropdownStatus
+    if (value || value === '') {
+      const filterValue = value.toLowerCase()
+
+      if (key === 'country') {
+        return this.masterNationalities.filter((option: string) =>
+          option.toLowerCase().includes(filterValue))
+      } if (key === 'state') {
+        return this.statesArr.filter((option: string) =>
+          option.toLowerCase().includes(filterValue))
+      } if (key === 'district') {
+        return this.districtArr.filter((option: string) =>
+          option.toLowerCase().includes(filterValue))
+      }
+
+    }
+    return []
+  }
+
+  getSelectedData(data: any) {
+    let preText = ''
+    const content = `${this.createChatForm.value.replymsg}, `
+    preText = this.createChatForm.value.replymsg
+      ? content
+      : this.createChatForm.value.replymsg
+
+    const text = preText + data
+    this.createChatForm.patchValue({ replymsg: text })
+
+    if (data.toLowerCase() === 'india') {
+      this.dropdownStatus = 'state'
+      const array: any = []
+      const obsof1 = of(array)
+      this.filteredOptions = obsof1
+      this.councilFilter('')
+    } else {
+      this.dropdownStatus = ''
+      this.enableInputForDropdown = false
+    }
+
+  }
+
+  getSelectedDataState(data: any) {
+    let preText = ''
+
+    const content = `${this.createChatForm.value.replymsg}, `
+    preText = this.createChatForm.value.replymsg
+      ? content
+      : this.createChatForm.value.replymsg
+
+    const text = preText + data
+    this.createChatForm.patchValue({ replymsg: text })
+
+    const disList = this.district.filter((dis: any) => {
+      if (dis.state === data) {
+        return dis
+      }
+    })
+
+    this.districtArr = disList[0].districts
+
+    this.dropdownStatus = 'district'
+    const array: any = []
+    const obsof1 = of(array)
+    this.filteredOptions = obsof1
+    this.councilFilter('')
+
+  }
+
+  getSelectedDataDistrict(data: any) {
+    let preText = ''
+    const content = `${this.createChatForm.value.replymsg}, `
+    preText = this.createChatForm.value.replymsg
+      ? content
+      : this.createChatForm.value.replymsg
+
+    const text = preText + data
+    this.createChatForm.patchValue({ replymsg: text })
+    this.dropdownStatus = ''
+
   }
 
   getChatResponse(_chatFormValue: any) {
@@ -78,6 +229,13 @@ export class ChatbotComponent implements OnInit {
         message = _chatFormValue.replymsg
         this.inputMsgEnabled = true
       }
+
+      if (this.nextId === 'location') {
+        this.enableInputForDropdown = true
+        this.createChatForm.value.replymsg = ''
+        this.dropdownStatus = 'country'
+      }
+
       if (_chatFormValue.replymsg === 'Others - Please Specify') {
         this.inputMsgEnabled = false
         $('#user-input').val('')
@@ -259,8 +417,17 @@ export class ChatbotComponent implements OnInit {
 
     // console.log('OBjeee ', profileRequest)
 
-    this.userProfileSvc.updateProfileDetails(profileRequest).subscribe(() => {
-      // console.log('Result -----')
+    this.userProfileSvc.updateProfileDetails(profileRequest).subscribe(data => {
+      if (data) {
+        // on Success
+        if (localStorage.getItem('selectedCourse')) {
+          let baseUrl = document.baseURI
+          baseUrl = document.baseURI + localStorage.getItem('selectedCourse')
+        } else {
+          this.router.navigate(['/page/home'])
+        }
+      }
+
     })
   }
 }
