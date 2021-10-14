4aeaf9300f66041cbfc12f16c0c5f1b9c1bd192f
sprakash22 <surya.prakash@tarento.com>
2021-10-14 15:32:15 +0530

    UI Changes ready for SPHER-91

--- library/ws-widget/collection/src/lib/btn-profile/btn-profile.component.ts
+++ library/ws-widget/collection/src/lib/btn-profile/btn-profile.component.ts
@@ -150,10 +150,10 @@ export class BtnProfileComponent extends WidgetBaseComponent
   public attachSignin(element: any) {
     this.auth2.attachClickHandler(element, {},
       (googleUser: any) => {
+         /* tslint:disable:no-unused-variable */
         const profile = googleUser.getBasicProfile()
         // console.log('Token || ' + googleUser.getAuthResponse().id_token)
-        // tslint:disable-next-line:no-console
-        console.log(`'ID: ' + profile.getId()`)
+        //console.log(`'ID: ' + profile.getId()`)
         // console.log('Name: ' + profile.getName())
         // console.log('Image URL: ' + profile.getImageUrl())
         // console.log('Email: ' + profile.getEmail())
