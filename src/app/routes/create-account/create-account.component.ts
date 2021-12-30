9b8482fb48b58daf568ef7e3a4b21e2b76e8530e
AnkitaTarento <ankita.kaushik@tarento.com>
2021-12-30 13:36:39 +0530

    add home router redirection

--- src/app/routes/create-account/create-account.component.ts
+++ src/app/routes/create-account/create-account.component.ts
@@ -44,7 +44,7 @@ export class CreateAccountComponent implements OnInit {
       password: new FormControl('', [Validators.required,
       Validators.pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})/g)]),
       confirmPassword: new FormControl('', [Validators.required]),
-    }, { validator: mustMatch('password', 'confirmPassword') })
+    },                                                   { validator: mustMatch('password', 'confirmPassword') })
 
     this.otpCodeForm = this.spherFormBuilder.group({
       otpCode: new FormControl('', [Validators.required]),
@@ -122,7 +122,7 @@ export class CreateAccountComponent implements OnInit {
           this.openSnackbar(res.msg)
         }
       },
-        err => {
+                                                  err => {
           this.openSnackbar(err.msg)
           this.uploadSaveData = false
           // form.reset()
@@ -145,7 +145,7 @@ export class CreateAccountComponent implements OnInit {
           this.openSnackbar(res.msg)
         }
       },
-        err => {
+                                                                   err => {
           this.errors = err
           this.openSnackbar(this.errors.msg)
           this.uploadSaveData = false
