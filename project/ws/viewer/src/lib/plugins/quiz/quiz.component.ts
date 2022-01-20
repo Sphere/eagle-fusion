b7b0067536c1d7f75aeb5e6bfcd52d56b60a8c79
sprakash22 <surya.prakash@tarento.com>
2022-01-20 17:40:04 +0530

    Fix for assessment

--- project/ws/viewer/src/lib/plugins/quiz/quiz.component.ts
+++ project/ws/viewer/src/lib/plugins/quiz/quiz.component.ts
@@ -172,7 +172,9 @@ export class QuizComponent implements OnInit, OnChanges, OnDestroy {
         })
     }
   }
-
+reTakeQuiz() {
+    this.startQuiz()
+}
   fillSelectedItems(question: NSQuiz.IQuestion, optionId: string) {
     this.raiseTelemetry('mark', optionId, 'click')
     if (this.viewState === 'answer') {
@@ -248,10 +250,10 @@ export class QuizComponent implements OnInit, OnChanges, OnDestroy {
       this.questionAnswerHash,
     )
     const sanitizedRequestData: NSQuiz.IQuizSubmitRequest = this.quizSvc.sanitizeAssessmentSubmitRequest(requestData)
-    sanitizedRequestData["artifactUrl"] = this.artifactUrl
+    sanitizedRequestData['artifactUrl'] = this.artifactUrl
     this.quizSvc.submitQuizV2(sanitizedRequestData).subscribe(
       (res: NSQuiz.IQuizSubmitResponse) => {
-        window.scrollTo(0,0);
+        window.scrollTo(0, 0)
         if (this.quizJson.isAssessment) {
           this.isIdeal = true
         }
