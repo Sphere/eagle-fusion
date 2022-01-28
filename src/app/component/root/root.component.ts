b3e7ece8f2c10c7b64b62e745bea7fe56e527af1
sprakash22 <surya.prakash@tarento.com>
2022-01-28 14:37:42 +0530

    UI fixes

--- src/app/component/root/root.component.ts
+++ src/app/component/root/root.component.ts
@@ -114,6 +114,7 @@ export class RootComponent implements OnInit, AfterViewInit {
         }
       }
       if (event instanceof NavigationStart) {
+          // tslint:disable-next-line: max-line-length
         if (event.url.includes('preview') || event.url.includes('embed') || event.url.includes('/public/register') || event.url.includes('/app/org-details')) {
           this.isNavBarRequired = false
         } else if (event.url.includes('author/') && this.isInIframe) {
