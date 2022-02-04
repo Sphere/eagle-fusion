336bd4646a38ad93019c1612a3f1f63c4cf6dfed
sprakashg22 <89121816+sprakashg22@users.noreply.github.com>
2022-02-04 14:46:19 +0530

    Certificate issue api

--- project/ws/app/src/lib/routes/app-toc/components/app-toc-banner/app-toc-banner.component.ts
+++ project/ws/app/src/lib/routes/app-toc/components/app-toc-banner/app-toc-banner.component.ts
@@ -308,13 +308,26 @@ export class AppTocBannerComponent implements OnInit, OnChanges, OnDestroy {
           userIds: [userId],
         },
       }
-      this.contentSvc.processCertificate(req).toPromise().then((response: any) => {
-        if (response.params.errmsg) {
+      this.contentSvc.processCertificate(req).subscribe((response: any) => {
+        if (response.responseCode === 'OK') {
+          this.sendApi()
+        } else {
           this.displayStyle = 'block'
         }
-      })
+      },
+        err => {
+          this.displayStyle = 'block'
+          /* tslint:disable-next-line */
+          console.log(err.error.params.errmsg)
+          //this.openSnackbar(err.error.params.errmsg)
+        })
     }
-
+  }
+  sendApi() {
+    let userId
+    if (this.configSvc.userProfile) {
+      userId = this.configSvc.userProfile.userId || ''
+    }
     this.contentSvc.fetchUserBatchList(userId).subscribe(
       (courses: NsContent.ICourse[]) => {
         // let enrolledCourse: NsContent.ICourse | undefined
@@ -346,13 +359,8 @@ export class AppTocBannerComponent implements OnInit, OnChanges, OnDestroy {
           }
         }
       })
-
-    // this.displayStyle = 'block'
-    // this.contentSvc.downloadCertificateAPI(content.identifier).toPromise().then((data: any) => {
-    //   // tslint:disable-next-line:no-console
-    //   console.log(data)
-    // })
   }
+
   closePopup() {
     this.displayStyle = 'none'
   }
@@ -686,7 +694,7 @@ export class AppTocBannerComponent implements OnInit, OnChanges, OnDestroy {
               const query = this.generateQuery('START')
               this.router.navigate([this.firstResourceLink.url], { queryParams: query })
             }
-          },         500)
+          }, 500)
 
         } else {
           this.openSnackbar('Something went wrong, please try again later!')
