93ad3cdda4819ef5d806e42358a4d21d09846396
AnkitaTarento <ankita.kaushik@tarento.com>
2022-02-23 14:42:52 +0530

    whatsapp share

---
+++ library/ws-widget/collection/src/lib/btn-whatsapp-share/btn-whatsapp-share.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core'
+import { CommonModule } from '@angular/common'
+import { BtnWhatsappShareComponent } from './btn-whatsapp-share/btn-whatsapp-share.component'
+
+@NgModule({
+  declarations: [BtnWhatsappShareComponent],
+  imports: [CommonModule],
+  exports: [BtnWhatsappShareComponent],
+})
+export class BtnWhatsappShareModule { }
