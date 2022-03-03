3e5be6e0d69ab552d6c047fbb0b1c5ab66dd0589
amantarento <amankumar.sharma@tarento.com>
2022-03-03 15:36:52 +0530

    validate certificate service ,component lint error fix

---
+++ src/app/routes/certificate/certificate.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core'
+import { CommonModule } from '@angular/common'
+import { CertificateRoutingModule } from './certificate-routing.module'
+import { FormsModule } from '@angular/forms'
+import { CertificateDetailsComponent } from './components/index'
+import { CertificateService } from './services/certificate.service'
+import { ApiService } from '@ws/author/src/public-api'
+import { MatIconModule } from '@angular/material'
+
+@NgModule({
+  declarations: [
+    CertificateDetailsComponent,
+  ],
+  imports: [
+    CommonModule,
+    CertificateRoutingModule,
+    FormsModule,
+    MatIconModule,
+  ],
+  providers: [CertificateService, ApiService],
+})
+export class CertificateModule { }
