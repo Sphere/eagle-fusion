48c5ce209e7352cd7e46ff53ac089fafc3bd94ba
Ankit <ankitv89@gmail.com>
2022-03-31 15:49:08 +0530

    WRAI course hidden

--- library/ws-widget/collection/src/lib/content-strip-multiple/content-strip-multiple.component.ts
+++ library/ws-widget/collection/src/lib/content-strip-multiple/content-strip-multiple.component.ts
@@ -614,7 +614,7 @@ export class ContentStripMultipleComponent extends WidgetBaseComponent
   filterCourse(contents: any) {
     const list = contents.content
     const newList = list.filter((i: any) => {
-      return i.identifier !== 'do_11347222086063718413'
+      return i.identifier !== 'do_1135049851060633601604'
     })
     contents.content = newList
     return contents
