b5fd165a453ddb32dba2eac33b28bed47370badb
amantarento <amankumar.sharma@tarento.com>
2022-04-07 10:53:49 +0530

    before taking pull

--- src/app/routes/profile-view/education-list/education-list.component.ts
+++ src/app/routes/profile-view/education-list/education-list.component.ts
@@ -1,13 +1,26 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core'
+import { ConfigurationsService } from '../../../../../library/ws-widget/utils/src/public-api'
+import { UserProfileService } from '../../../../../project/ws/app/src/lib/routes/user-profile/services/user-profile.service'
+import * as _ from 'lodash'
 @Component({
   selector: 'ws-education-list',
   templateUrl: './education-list.component.html',
   styleUrls: ['./education-list.component.scss']
 })
 export class EducationListComponent implements OnInit {
-
-  constructor() { }
+  academicsArray: any[] = []
+  constructor(private configSvc: ConfigurationsService,
+    private userProfileSvc: UserProfileService,) {
+    if (this.configSvc.userProfile) {
+      this.userProfileSvc.getUserdetailsFromRegistry(this.configSvc.unMappedUser.id).subscribe(
+        (data: any) => {
+          if (data && _.get(data, 'profileDetails.profileReq.academics')) {
+            this.academicsArray = _.get(data, 'profileDetails.profileReq.academics')
+            console.log(this.academicsArray)
+          }
+        })
+    }
+  }
 
   ngOnInit() {
   }
