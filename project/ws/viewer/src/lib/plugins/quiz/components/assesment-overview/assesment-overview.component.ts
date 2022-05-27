996cc3ab593272983fd7486858aeb2f865048859
Vishali Sakar <vishali.sakar@tarento.com>
2022-05-27 10:59:02 +0530

    assesment overview component added

---
+++ project/ws/viewer/src/lib/plugins/quiz/components/assesment-overview/assesment-overview.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'viewer-assesment-overview',
+  templateUrl: './assesment-overview.component.html',
+  styleUrls: ['./assesment-overview.component.scss']
+})
+export class AssesmentOverviewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
