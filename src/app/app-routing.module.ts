3ea0eaccf9f445902b21933f1c1a1b757ec14d80
sprakashg22 <89121816+sprakashg22@users.noreply.github.com>
2022-07-18 12:48:14 +0530

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -40,7 +40,6 @@ import { EducationEditComponent } from './routes/profile-view/education-edit/edu
 import { WorkInfoListComponent } from './routes/profile-view/work-info-list/work-info-list.component'
 import { WorkInfoEditComponent } from './routes/profile-view/work-info-edit/work-info-edit.component'
 import { PersonalDetailEditComponent } from './routes/profile-view/personal-detail-edit/personal-detail-edit.component'
-
 // 💥💥💥💥💥💥💥💥💥💥💥💥💥💥💥💥💥💥💥💥💥💥💥💥💥💥💥💥💥💥💥💥💥💥💥💥💥💥💥
 // Please declare routes in alphabetical order
 // 😵😵😵😵😵😵😵😵😵😵😵😵😵😵😵😵😵😵😵😵😵😵😵😵😵😵😵😵😵😵😵😵😵😵😵😵😵😵😵
