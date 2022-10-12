f1bc2debaf90738fc88a44b7492035ec8fc6c0f2
amantarento <amankumar.sharma@tarento.com>
2022-10-12 17:04:25 +0530

    public route for keyclock

---
+++ src/app/routes/public/keycloak-callback/keycloak-callback.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ws-keycloak-callback',
+  templateUrl: './keycloak-callback.component.html',
+  styleUrls: ['./keycloak-callback.component.scss']
+})
+export class KeycloakCallbackComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
