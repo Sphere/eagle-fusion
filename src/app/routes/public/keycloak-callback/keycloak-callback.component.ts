7a835415fc4ae4eb8dcf287f79b6c99479a30ffd
sprakashg22 <89121816+sprakashg22@users.noreply.github.com>
2022-10-18 13:10:36 +0530

    check for read api

--- src/app/routes/public/keycloak-callback/keycloak-callback.component.ts
+++ src/app/routes/public/keycloak-callback/keycloak-callback.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core'
 import { MatSnackBar } from '@angular/material'
 import { OrgServiceService } from '../../../../../project/ws/app/src/lib/routes/org/org-service.service'
-
+import { SignupService } from 'src/app/routes/signup/signup.service'
 @Component({
   selector: 'ws-keycloak-callback',
   templateUrl: './keycloak-callback.component.html',
@@ -9,7 +9,8 @@ import { OrgServiceService } from '../../../../../project/ws/app/src/lib/routes/
 })
 export class KeycloakCallbackComponent implements OnInit {
 
-  constructor(private orgService: OrgServiceService, private snackBarSvc: MatSnackBar,) { }
+  constructor(private orgService: OrgServiceService, private snackBarSvc: MatSnackBar,
+    private signupService: SignupService, private snackBar: MatSnackBar) { }
 
 
 
@@ -24,15 +25,26 @@ export class KeycloakCallbackComponent implements OnInit {
     const code = sessionStorage.getItem('code') || null
     // console.log(code)
     try {
-      this.orgService.setConnectSid(code).subscribe((res: any) => {
+      this.orgService.setConnectSid(code).subscribe(async (res: any) => {
         if (res) {
           // console.log(res)
           sessionStorage.clear()
-          if (localStorage.getItem('url_before_login')) {
-            location.href = localStorage.getItem('url_before_login') || ''
-          } else {
-            location.href = '/page/home'
+          const result = await this.signupService.fetchStartUpDetails()
+          // tslint:disable-next-line:no-console
+          console.log(result)
+          if (result.status === 200 && result.roles.length > 0) {
+            this.openSnackbar('logged in')
+            if (localStorage.getItem('url_before_login')) {
+              location.href = localStorage.getItem('url_before_login') || ''
+            } else {
+              location.href = '/page/home'
+            }
           }
+          // if (localStorage.getItem('url_before_login')) {
+          //   location.href = localStorage.getItem('url_before_login') || ''
+          // } else {
+          //   location.href = '/page/home'
+          // }
         }
       }, (err: any) => {
         // console.log(err)
@@ -46,6 +58,11 @@ export class KeycloakCallbackComponent implements OnInit {
       // alert('Error Occured while logging in')
       location.href = "/public/home"
     }
+  }
+  private openSnackbar(primaryMsg: string, duration: number = 3000) {
+    this.snackBar.open(primaryMsg, undefined, {
+      duration,
+    })
   }
 
 }
