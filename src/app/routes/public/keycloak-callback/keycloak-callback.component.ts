0fde7a8c2985f921cdb4c03f1d45ae626fa656c6
sprakashg22 <89121816+sprakashg22@users.noreply.github.com>
2022-10-21 14:41:22 +0530

    Fix for SBT-222

--- src/app/routes/public/keycloak-callback/keycloak-callback.component.ts
+++ src/app/routes/public/keycloak-callback/keycloak-callback.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core'
 import { MatSnackBar } from '@angular/material'
 import { OrgServiceService } from '../../../../../project/ws/app/src/lib/routes/org/org-service.service'
 import { SignupService } from 'src/app/routes/signup/signup.service'
-import { HttpClient } from '@angular/common/http'
 @Component({
   selector: 'ws-keycloak-callback',
   templateUrl: './keycloak-callback.component.html',
@@ -11,28 +10,14 @@ import { HttpClient } from '@angular/common/http'
 export class KeycloakCallbackComponent implements OnInit {
   isLoading = false
   constructor(private orgService: OrgServiceService, private snackBarSvc: MatSnackBar,
-    private signupService: SignupService, private http: HttpClient,
+    private signupService: SignupService,
   ) { }
 
 
 
   async ngOnInit() {
-    let res
-    try {
-      res = await this.http.get('/apis/proxies/v8/logout/user').toPromise()
-      localStorage.setItem('loggedout', JSON.stringify(res))
-      localStorage.removeItem('telemetrySessionId')
-      localStorage.removeItem('loginbtn')
-      localStorage.removeItem('url_before_login')
-      localStorage.removeItem('tocData')
-      localStorage.removeItem(`userUUID`)
-      // const url = `${document.baseURI}public/home`
-      // const Keycloakurl = `${document.baseURI}auth/realms/sunbird/protocol/openid-connect/logout?redirect_uri=${encodeURIComponent(url)}`
-      // window.location.href = Keycloakurl
-    } catch (error) { }
-
     const loginBtn = sessionStorage.getItem('login-btn') || null
-    if (loginBtn === 'clicked' && res) {
+    if (loginBtn === 'clicked') {
       this.isLoading = true
       this.checkKeycloakCallback()
     }
