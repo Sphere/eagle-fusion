ea50a0866bd2e3160d9bd3f44541573af95b1efb
Saurav Verma <saurav.verma@tarento.com>
2022-11-08 00:09:56 +0530

    E-abhyas conflicts resolved

--- src/organisations/org-home/org-home.component.ts
+++ src/organisations/org-home/org-home.component.ts
@@ -33,7 +33,7 @@ export class OrgHomeComponent implements OnInit {
         // tslint:disable-next-line:max-line-length
         enrollmentArr = responseEnrollment.map((identifierValue: { contentId: any, courseName: any, courseLogoUrl: any }) => [{ identifier: identifierValue.contentId, name: identifierValue.courseName, logo: identifierValue.courseLogoUrl }])
 
-        // this.resultEnroll = enrollmentArr
+        this.resultEnroll = enrollmentArr
       })
     }
     console.log('enrollmentArr array >>>>>>>>...' + enrollmentArr)
