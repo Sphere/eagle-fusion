19d4053b7742cb21f3c05c1db1acf41b0b8b51f2
amantarento <amankumar.sharma@tarento.com>
2022-12-22 14:23:06 +0530

    change in assessment-modal

--- project/ws/viewer/src/lib/routes/quiz/quiz.component.ts
+++ project/ws/viewer/src/lib/routes/quiz/quiz.component.ts
@@ -100,8 +100,10 @@ export class QuizComponent implements OnInit, OnDestroy {
 
   private async transformQuiz(content: NsContent.IContent): Promise<NSQuiz.IQuiz> {
     if (this.activatedRoute.snapshot.queryParams.competency) {
-      const artifactUrl = this.viewSvc.getCompetencyAuthoringUrl(content.artifactUrl.split('/content')[1]
-      )
+      const artifactUrl = this.forPreview
+        ? this.viewSvc.getCompetencyAuthoringUrl(content.artifactUrl.split('/content')[1]
+        )
+        : content.artifactUrl
       let quizJSON: NSQuiz.IQuiz = await this.http
         .get<any>(artifactUrl || '')
         .toPromise()
