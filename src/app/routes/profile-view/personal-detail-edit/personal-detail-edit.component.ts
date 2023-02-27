663f16e1b529e5ec4667beba508baf647a38986b
sprakashg22 <89121816+sprakashg22@users.noreply.github.com>
2023-02-27 15:12:51 +0530

    fix for lang label selection

--- src/app/routes/profile-view/personal-detail-edit/personal-detail-edit.component.ts
+++ src/app/routes/profile-view/personal-detail-edit/personal-detail-edit.component.ts
@@ -175,9 +175,16 @@ export class PersonalDetailEditComponent implements OnInit, AfterViewInit, After
           if (data) {
             this.userProfileData = data.profileDetails.profileReq
             this.updateForm()
-            this.personalDetailForm.patchValue({
-              knownLanguage: data.profileDetails.preferences.language || 'en',
-            })
+            if (data.profileDetails.preferences!.language === 'hi') {
+              this.personalDetailForm.patchValue({
+                knownLanguage: 'हिंदी',
+              })
+            } else {
+              this.personalDetailForm.patchValue({
+                knownLanguage: 'English',
+              })
+            }
+
             this.populateChips(this.userProfileData)
           }
         })
@@ -292,7 +299,6 @@ export class PersonalDetailEditComponent implements OnInit, AfterViewInit, After
       data.professionalDetails[0].orgType === 'Others' ? this.orgOthersField = true : this.orgOthersField = false
       data.professionalDetails[0].profession === 'Others' ? this.professionOtherField = true : this.professionOtherField = false
       data.professionalDetails[0].profession === 'Healthcare Worker' ? this.rnShow = true : this.rnShow = false
-
       this.personalDetailForm.patchValue({
         // userName: this.profileUserName,
         firstname: data.personalDetails.firstname,
@@ -387,6 +393,7 @@ export class PersonalDetailEditComponent implements OnInit, AfterViewInit, After
       this.preferedLanguage = result
       this.personalDetailForm.controls.
         knownLanguage.setValue(_.upperFirst(result.lang))
+
       if (this.configSvc.userProfileV2) {
         let user: any
         const userid = this.configSvc.userProfileV2.userId
