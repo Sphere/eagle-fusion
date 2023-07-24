95643d2bac348cff84f8a3ab18e6880ab65cb95d
Surya Prakash G <surya.prakash@tarento.com>
2023-07-24 16:40:12 +0530

    fix for sbt-656

--- src/app/component/root/root.component.ts
+++ src/app/component/root/root.component.ts
@@ -266,6 +266,7 @@ export class RootComponent implements OnInit, AfterViewInit {
           sessionStorage.setItem('login-btn', 'clicked')
 
           setTimeout(() => {
+
             this.signupService.fetchStartUpDetails().then(result => {
               if (result && result.status !== 200) {
                 //this.authSvc.logout()
@@ -280,12 +281,12 @@ export class RootComponent implements OnInit, AfterViewInit {
                   redirectUrl = `${url}openid/keycloak`
                 }
                 // tslint:disable-next-line:max-line-length
-                const keycloakurl = `${document.baseURI}auth/realms/sunbird/protocol/openid-connect/auth?client_id=portal&redirect_uri=${encodeURIComponent(redirectUrl)}&state=${state}&response_mode=fragment&response_type=code&scope=openid&nonce=${nonce}`
+                const keycloakurl = `${url}auth/realms/sunbird/protocol/openid-connect/auth?client_id=portal&redirect_uri=${encodeURIComponent(redirectUrl)}&state=${state}&response_mode=fragment&response_type=code&scope=openid&nonce=${nonce}`
                 window.location.href = keycloakurl
               }
             })
 
-          }, 1000)
+          }, 100)
           // if (this.configSvc.userProfile === null) {
           //   localStorage.setItem(`url_before_login`, `app/toc/` + `${_.split(event.url, '/')[3]
           //     }` + `/overview`)
