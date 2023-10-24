74dab5e214f581bca39f1e337255253ec7d78fbc
Surya Prakash G <surya.prakash@tarento.com>
2023-10-24 18:13:48 +0530

    fixes for spher-993

--- src/app/routes/web-nav-link/web-nav-link-page.component.ts
+++ src/app/routes/web-nav-link/web-nav-link-page.component.ts
@@ -17,15 +17,22 @@ export class WebNavLinkPageComponent implements OnInit {
     private signupService: SignupService,
     location: Location,
   ) {
+    console.log('urlchanges')
     if (location.path().includes('/app/profile-view')) {
-      this.showFontColor = true
+      this.showProfile = true
+      this.showHome = false
+    } else if (location.path().includes('/page/home')) {
+      this.showHome = true
     } else {
-      this.showFontColor = false
+      this.showCompetency = true
+      this.showHome = false
     }
   }
   linksData: any
   data: any
-  showFontColor = false
+  showHome = true
+  showCompetency = false
+  showProfile = false
   ngOnInit() {
     console.log(this.router.url)
     this.data = this.configSvc.unMappedUser!
@@ -47,24 +54,41 @@ export class WebNavLinkPageComponent implements OnInit {
       },
     ]
   }
-  async redirect() {
-    console.log(this.configSvc.unMappedUser!.profileDetails!.profileReq!.personalDetails!)
-    let result = await this.signupService.getUserData()
-    console.log(result)
-    if (result && result.profileDetails!.profileReq!.personalDetails!.dob) {
-      this.showFontColor = true
-      location.href = '/app/profile-view'
+
+  async redirect(text: string) {
+    let local = (this.configSvc.unMappedUser && this.configSvc.unMappedUser!.profileDetails && this.configSvc.unMappedUser!.profileDetails!.preferences && this.configSvc.unMappedUser!.profileDetails!.preferences!.language !== undefined) ? this.configSvc.unMappedUser.profileDetails.preferences.language : location.href.includes('/hi/') === true ? 'hi' : 'en'
+    let url1 = local === 'hi' ? 'hi' : ""
+    console.log(url1)
+    if (text === 'home') {
+      this.showProfile = false
+      this.showHome = true
+      let url = '/page/home'
+      location.href = `${url1}${url}`
+    } else if (text === 'competency') {
+      this.showCompetency = true
+      this.showProfile = false
+      let url = '/app/user/competency'
+      location.href = `${url1}${url}`
     } else {
-      console.log('p')
-      this.showFontColor = true
-      if (localStorage.getItem('url_before_login')) {
-        const courseUrl = localStorage.getItem('url_before_login')
-        // const url = `app/about-you`
-        this.router.navigate(['/app/about-you'], { queryParams: { redirect: courseUrl } })
-        // window.location.assign(`${location.origin}/${this.lang}/${url}/${courseUrl}`)
+      console.log(this.configSvc.unMappedUser!.profileDetails!.profileReq!.personalDetails!)
+      let result = await this.signupService.getUserData()
+      console.log(result)
+      if (result && result.profileDetails!.profileReq!.personalDetails!.dob) {
+        this.showProfile = true
+        let url = '/app/profile-view'
+        location.href = `${url1}${url}`
       } else {
-        const url = '/page/home'
-        this.router.navigate(['/app/about-you'], { queryParams: { redirect: url } })
+        console.log('p')
+        this.showProfile = true
+        if (localStorage.getItem('url_before_login')) {
+          const courseUrl = localStorage.getItem('url_before_login')
+          // const url = `app/about-you`
+          this.router.navigate(['/app/about-you'], { queryParams: { redirect: courseUrl } })
+          // window.location.assign(`${location.origin}/${this.lang}/${url}/${courseUrl}`)
+        } else {
+          const url = '/page/home'
+          this.router.navigate(['/app/about-you'], { queryParams: { redirect: `${url1}${url}` } })
+        }
       }
     }
   }
