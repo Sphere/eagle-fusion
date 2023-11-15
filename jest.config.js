b3e5d02401e996f13854c7cac849364b07a97eaa
Surya Prakash G <surya.prakash@tarento.com>
2023-11-15 16:45:31 +0530

    Config changes for Unit testing

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+// require('jest-preset-angular/ngcc-jest-processor');
+
+globalThis.ngJest = {
+  skipNgcc: true,
+  tsconfig: 'tsconfig.spec.json', // this is the project root tsconfig
+}
+
+/** @type {import('@jest/types').Config.InitialOptions} */
+module.exports = {
+  preset: 'jest-preset-angular',
+  setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
+}
