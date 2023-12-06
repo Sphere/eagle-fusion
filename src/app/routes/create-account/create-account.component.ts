cb79a64cdae5c947691e51c5e26f028a3045a705
likhithgowda <likhith.thammegowda@tarento.com>
2023-12-06 09:03:58 +0530

    fix for translation

--- src/app/routes/create-account/create-account.component.ts
+++ src/app/routes/create-account/create-account.component.ts
@@ -64,21 +64,7 @@ export class CreateAccountComponent implements OnInit {
       }
 
       if (lang.id === 'hi') {
-        const langs = lang.id
-        if (this.router.url.includes('hi')) {
-          const lan = this.router.url.split('hi/').join('')
-          if (lang === 'hi') {
-            window.location.assign(`${location.origin}/${langs}${lan}`)
-          } else {
-            window.location.assign(`${location.origin}${langs}${lan}`)
-          }
-        } else {
-          if (lang === 'hi') {
-            window.location.assign(`${location.origin}/${langs}${this.router.url}`)
-          } else {
-            window.location.assign(`${location.origin}${langs}${this.router.url}`)
-          }
-        }
+        window.location.assign(`${location.origin}/${lang}${this.router.url}`)
       }
     }
     // this.spherFormBuilder = spherFormBuilder
