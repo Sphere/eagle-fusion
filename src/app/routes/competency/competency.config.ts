15a7ab32d06a03bf949eb0e1abe107e2861fe36d
likhithgowda <likhith.thammegowda@tarento.com>
2024-12-03 10:15:50 +0530

    package load issue

--- src/app/routes/competency/competency.config.ts
+++ src/app/routes/competency/competency.config.ts
@@ -1,3 +1,22 @@
 export const COMPETENCY_REGISTRATION_CONFIG = {
-  config: JSON.parse(localStorage.getItem('competency') || '{}'),
+  config: {
+    "userName": "ankitakaushik_s9u3",
+    "profileData": [
+      {
+        "name": "asasd",
+        "orgType": "Public/Government Sector",
+        "designation": "GNM",
+        "profession": "Healthcare Worker",
+        "responsibilities": "",
+        "completePostalAddress": "",
+        "additionalAttributes": {},
+        "nameOther": "",
+        "locationselect": null,
+        "organizationName": "test ehereere"
+      }
+    ],
+    "language": "en",
+    "id": "6678e8ac-7e76-4af3-8354-c0970dd78419",
+    "hostPath": "aastrika-stage.tarento.com"
+  },
 }
