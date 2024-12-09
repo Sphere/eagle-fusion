25996d02651a6c1437a8f9de65883a0cb27a5c90
likhithgowda <likhith.thammegowda@tarento.com>
2024-12-09 13:20:01 +0530

    Certificate download size issue fix

--- project/ws/app/src/lib/routes/app-toc/components/app-toc-certificate-modal/app-toc-certificate-modal.component.ts
+++ project/ws/app/src/lib/routes/app-toc/components/app-toc-certificate-modal/app-toc-certificate-modal.component.ts
@@ -70,19 +70,30 @@ export class AppTocCertificateModalComponent implements OnInit {
         const img = new Image()
         const name = this.content.tocConfig
         const url = await response.result.printUri
+        console.log("response", response.result)
         // this.img = this.sanitizer.bypassSecurityTrustUrl(url)
         this.isLoading = false
         img.onload = function () {
-
+          const defaultWidth = 1350
+          const defaultHeight = 880
           const canvas: any = document.getElementById('certCanvas') || {}
           const ctx = canvas.getContext('2d')
+          console.log("img.width", img.width)
           const imgWidth = img.width
           const imgHeight = img.height
-          canvas.width = imgWidth
-          canvas.height = imgHeight
-          ctx.drawImage(img, 0, 0, imgWidth, imgHeight)
+
+          if (imgWidth < 1000 && imgHeight < 600) {
+            canvas.width = defaultWidth
+            canvas.height = defaultHeight
+            ctx.drawImage(img, 0, 0, defaultWidth, defaultHeight)
+          } else {
+            canvas.width = imgWidth
+            canvas.height = imgHeight
+            ctx.drawImage(img, 0, 0, imgWidth, imgHeight)
+          }
           let imgURI = canvas
             .toDataURL('image/jpeg')
+          console.log("imgWidth", canvas.width, "imgHeight", canvas.height)
 
           imgURI = decodeURIComponent(imgURI.replace('data:image/jpeg,', ''))
           const arr = imgURI.split(',')
