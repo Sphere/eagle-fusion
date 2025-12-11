999546e5b58c3fdcde5387043726ef09b2320b3a
vpPavithra <pavithra.prakash@tarento.com>
2025-12-11 14:10:27 +0530

    registration form in service otp submit fix

--- src/app/routes/bnrc-login-otp/bnrc-login-otp.component.ts
+++ src/app/routes/bnrc-login-otp/bnrc-login-otp.component.ts
@@ -20,6 +20,7 @@ export class BnrcLoginOtpComponent implements OnInit {
   emailPhoneType: any = 'phone'
   loginVerification = false
   redirectUrl = ''
+  disableSubmit: boolean = false
   constructor(
     public router: Router,
     private fb: FormBuilder,
@@ -57,26 +58,29 @@ export class BnrcLoginOtpComponent implements OnInit {
         ? this.userProfileSvc.mpValidateOtp.bind(this.userProfileSvc)
         : this.userProfileSvc.bnrcValidateOtp.bind(this.userProfileSvc)
 
-
-    validateOtpMethod(request).subscribe(
-      (res: any) => {
-        if (res.status === 'success') {
-          this.openSnackbar(res.message.message)
-          this.redirectToParent.emit(res)
-          return res
+    if (!this.disableSubmit) {
+      this.disableSubmit = true
+      validateOtpMethod(request).subscribe(
+        (res: any) => {
+          if (res.status === 'success') {
+            this.openSnackbar(res.message.message)
+            this.redirectToParent.emit(res)
+            return res
+          }
+        },
+        (error: any) => {
+          this.disableSubmit = false
+          const errorMessage = error.error && error.error.message ? error.error.message : (error.message || 'An unexpected error occurred')
+          this.openSnackbar(errorMessage)  // Handle error response
         }
-      },
-      (error: any) => {
-        const errorMessage = error.error && error.error.message ? error.error.message : (error.message || 'An unexpected error occurred')
-        this.openSnackbar(errorMessage)  // Handle error response
-      }
-    )
+      )
+    }
 
   }
 
   resendOTP() {
     this.isLoading = true
-
+    this.disableSubmit = false
     const request = {
       phone: this.loginData.value.phone,
     }
