58ed8616669b853c57f336bde70eb27cd92939e5
vpPavithra <pavithra.prakash@tarento.com>
2026-03-03 13:07:10 +0530

    updated competency data for you section as courses for home page with new api integration for ekshamata

--- src/app/routes/web-public-container/web-public-container.component.ts
+++ src/app/routes/web-public-container/web-public-container.component.ts
@@ -5,10 +5,12 @@ import { MatDialog } from '@angular/material/dialog'
 import { OrgServiceService } from '../../../../project/ws/app/src/lib/routes/org/org-service.service'
 import { ScrollService } from '../../services/scroll.service'
 import { ConfigurationsService, LoggerService, ValueService } from '@ws-widget/utils'
-import { forkJoin } from 'rxjs'
+import { forkJoin, Observable, of } from 'rxjs'
 import { PlaylistService } from '../../services/playlist.service'
 import { LanguageService } from '../../services/language.service'
 import { Subject } from 'rxjs'
+import { WidgetContentService } from '../../../../library/ws-widget/collection/src/public-api'
+import { catchError, map } from 'rxjs/operators'
 
 @Component({
   selector: 'ws-web-public-container',
@@ -49,6 +51,11 @@ export class WebPublicComponent implements OnInit, OnDestroy {
   uiConfig: any
   lang: string = ''
   isXSmall: boolean = false
+
+  currentOffset = 0
+  pageLimit = 500
+  initialPageLimit = 10
+  plyLsData: any[] = []
   constructor(
     private router: Router,
     public dialog: MatDialog,
@@ -58,7 +65,8 @@ export class WebPublicComponent implements OnInit, OnDestroy {
     private playlistSvc: PlaylistService,
     private langSvc: LanguageService,
     private valueSvc: ValueService,
-    private logger: LoggerService
+    private logger: LoggerService,
+    private contentSvc: WidgetContentService
   ) {
     this.lang = this.langSvc.getCurrentLanguage()
     effect(() => {
@@ -69,46 +77,181 @@ export class WebPublicComponent implements OnInit, OnDestroy {
   async ngOnInit() {
     this.handleScrollEvents()
     let designation = this.configSvc?.unMappedUser?.profileDetails?.profileReq?.professionalDetails?.[0]?.designation || ''
-    if (this.configSvc.userProfile) {
-      let plyLsData: any = await this.playlistSvc.getPlaylistConfig()
-      this.logger.log("plyLsData", plyLsData)
-      let res = this.configData
-      if (res?.length > 0)
-        this.uiConfig = res?.slice(1, -1)
-      plyLsData.forEach(async (element: any) => {
-        if (element.orgId == this.configSvc.userProfile.rootOrgId && element.language == this.lang) {
-          if (designation && element.role.map(role => role.toLowerCase()).includes(designation.toLowerCase()) && element.playlistId === "YOUR_PLANS_PLAYLIST") {
-            this.yourPlansCourseIdentifier = element.dataSource.payload
-          }
-          if (element.playlistId === "TOP_COURSE_PLAYLIST") {
-            this.topCertifiedCourseIdentifier = element.dataSource.payload
-          }
-          if (element.playlistId === "CNE_COURSE_PLAYLIST") {
-            this.cneCoursesIdentifier = element.dataSource.payload
-          }
-          if (this.isEkshamata) {
-            if (element.playlistId === "FEATURED_COURSE_PLAYLIST") {
-              this.featuredCourseIdentifier = element.dataSource.payload
-            }
+    const designationLower = designation.toLowerCase()
+    const rootOrgId = this.configSvc?.userProfile?.rootOrgId
+    const roleCheck = (roles: string[]) =>
+      roles?.some(r => r.toLowerCase() === designationLower)
+    if (this.configData) {
+      this.uiConfig = this.configData.slice(1, -1)
+    }
+    if (this.configSvc?.userProfile) {
+      this.plyLsData = await this.playlistSvc.getPlaylistConfig()
+      this.logger.log("plyLsData", this.plyLsData)
+
+      for (const element of this.plyLsData) {
+        if (element.orgId !== rootOrgId || element.language !== this.lang) continue
+        const { playlistId, dataSource } = element
+
+        if (designation && roleCheck(element.role)) {
+          if (playlistId === 'YOUR_PLANS_PLAYLIST') {
+            this.yourPlansCourseIdentifier = dataSource.payload
           }
         }
-      })
-    } else {
-      let res = this.configData
-      if (res) {
-        this.uiConfig = res.slice(1, -1)
+        if (playlistId === 'TOP_COURSE_PLAYLIST') {
+          this.topCertifiedCourseIdentifier = dataSource.payload
+        }
+        if (playlistId === 'CNE_COURSE_PLAYLIST') {
+          this.cneCoursesIdentifier = dataSource.payload
+        }
+        if (this.isEkshamata && playlistId === 'FEATURED_COURSE_PLAYLIST') {
+          this.featuredCourseIdentifier = dataSource.payload
+        }
       }
-      if (this.uiConfig)
-        this.uiConfig?.forEach(data => {
-          if (data?.playlistConfigId == 'TOP_COURSE_PLAYLIST') {
-            this.topCertifiedCourseIdentifier = data.payload
-          } else if (data?.playlistConfigId == 'CNE_COURSE_PLAYLIST') {
-            this.cneCoursesIdentifier = data.payload
+    } else {
+      this.uiConfig?.forEach(data => {
+        if (data?.playlistConfigId == 'TOP_COURSE_PLAYLIST') {
+          this.topCertifiedCourseIdentifier = data.payload
+        } else if (data?.playlistConfigId == 'CNE_COURSE_PLAYLIST') {
+          this.cneCoursesIdentifier = data.payload
+        }
+      })
+    }
+    this.isLoading = true
+    // Main flow
+    if (this.yourPlansCourseIdentifier.length > 0) {
+      this.fetchEnvironmentConfigurations()
+      return
+    } else {
+      this.handleCompetencyFlow(rootOrgId, roleCheck)
+    }
+  }
+
+  private handleCompetencyFlow(rootOrgId: string, roleCheck: (roles: string[]) => boolean) {
+    const matchedElements = this.plyLsData?.filter(element =>
+      element.orgId === rootOrgId && roleCheck(element.role) && ((element.playlistId === 'COMPETENCY_PLAYLIST' && element.language === this.lang) || element.playlistId === 'SEARCH_PLAYLIST'))
+
+    const listOfEnrolledCourseId = (this.userEnrollCourse || [])
+      .filter(course => course?.content?.identifier && !course?.content?.competency)
+      .map(course => course.content.identifier)
+
+    let competencySearchArray: string[] = []
+    let baseQuery: any = {}
+    let sourceName: string[] = []
+
+    for (const element of matchedElements || []) {
+      const { playlistId, dataSource } = element
+
+      if (playlistId === 'COMPETENCY_PLAYLIST') {
+        competencySearchArray.push(
+          ...this.buildCompetencySearchArray(dataSource?.payload)
+        )
+      }
+      if (playlistId === 'SEARCH_PLAYLIST') {
+        baseQuery = dataSource?.payload || {}
+
+        baseQuery.request = baseQuery.request || {}
+        baseQuery.request.filters = baseQuery.request.filters || {}
+
+        baseQuery.request.offset = this.currentOffset
+        baseQuery.request.limit = this.pageLimit
+
+        sourceName = baseQuery.request.filters.sourceName || []
+      }
+    }
+    this.currentOffset += this.initialPageLimit
+    this.pageLimit += this.initialPageLimit
+    if (!competencySearchArray.length) {
+      this.isLoading = false
+      return
+    }
+
+    this.searchContentByCompetencies$(baseQuery, competencySearchArray, sourceName, listOfEnrolledCourseId).subscribe((res: any) => {
+      this.coursesForYou = res || []
+      this.yourPlansCourseIdentifier = this.coursesForYou.filter(item => item?.identifier).map(item => item.identifier)
+      this.updateCourseData()
+      this.isLoading = false
+    })
+  }
+
+  buildCompetencySearchArray = (competencyPayload: any[]): string[] => {
+    if (!Array.isArray(competencyPayload) || competencyPayload?.length === 0) {
+      return []
+    }
+    const competencySearchArray: string[] = []
+    competencyPayload.forEach((competencyObj) => {
+      Object.keys(competencyObj).forEach((key) => {
+        const competency = competencyObj[key]
+        const competencyId = competency?.id
+        if (!competencyId) return
+
+        const levelDescriptions = competency?.additionalProperties?.competencyLevelDescription || []
+
+        levelDescriptions.forEach((levelDesc: any) => {
+          const level = levelDesc?.level
+          if (level) {
+            competencySearchArray.push(`${competencyId}-${level}`)
           }
         })
-    }
-    this.fetchEnvironmentConfigurations()
+      })
+    })
+    return competencySearchArray
   }
+
+  searchContentByCompetencies$ = (baseQuery: any, competencySearchArray: string[], requiredSourceName: string[], listOfEnrolledCourseId: string[]): Observable<any[]> => {
+    if (!Array.isArray(competencySearchArray) || competencySearchArray.length === 0) {
+      return of([])
+    }
+
+    const requestBody = typeof structuredClone === 'function'
+      ? structuredClone(baseQuery)
+      : JSON.parse(JSON.stringify(baseQuery))
+
+    requestBody.request = requestBody.request || {}
+    requestBody.request.filters = requestBody.request.filters || {}
+    requestBody.request.filters.competencySearch = competencySearchArray
+
+    return this.contentSvc.getCouseByContentSearch(competencySearchArray, true, requestBody).pipe(
+      map((res: any) => {
+        const content = res?.result?.content ?? []
+
+        const processedCourses = this.processRecommendedCourses(content, requiredSourceName, listOfEnrolledCourseId)
+        return processedCourses
+      }),
+      catchError((err) => {
+        console.error("Error fetching recommendation", err)
+        return of([])
+      })
+    )
+  }
+
+  recommendedCourse = (data: any[]) =>
+    (data || [])
+      .filter(item => item && item.identifier)
+      .map((item) => ({
+        identifier: item.identifier,
+        appIcon: item.appIcon,
+        thumbnail: item.posterImage || item.thumbnail,
+        name: item.name,
+        sourceName: item.sourceName,
+        issueCertification: item.issueCertification,
+        averageRating: item.averageRating,
+        competency: item.competency,
+      }))
+
+
+  processRecommendedCourses = (courseList: any[], requiredSourceName: string[], listOfEnrolledCourseId: string[]): any[] => {
+    const seen = new Set()
+    const enrolledSet = new Set(listOfEnrolledCourseId || [])
+    const sourceSet = new Set(requiredSourceName || [])
+
+    return this.recommendedCourse(courseList).filter(item => {
+      const id = item?.identifier
+      if (!id || seen.has(id) || enrolledSet.has(id)) return false
+      if (!sourceSet.has(item.sourceName)) return false
+      seen.add(id)
+      return true
+    })
+  };
 
   private handleScrollEvents() {
     this.scrollService.scrollToDivEvent.subscribe((targetDivId: string) => {
@@ -133,14 +276,7 @@ export class WebPublicComponent implements OnInit, OnDestroy {
     ]
 
     const requests = !this.configSvc?.unMappedUser ? [this.orgService.getTopLiveSearchResults(defaultIds, 'en')] :
-      // (this.lang === 'en'
-      //   ? [
       this.orgService.getTopLiveSearchResults([...defaultIds, ...identifiers], this.lang)
-    // ]
-    // : [
-    //   this.orgService.getTopLiveSearchResults(defaultIds, 'en'),
-    //   this.orgService.getTopLiveSearchResults(identifiers, 'hi'),
-    // ])
 
     return forkJoin(requests).subscribe((responses: any[]) => {
       const content = responses
@@ -169,34 +305,37 @@ export class WebPublicComponent implements OnInit, OnDestroy {
         content.filter(item => featureSet.has(item.identifier)),
         'identifier'
       )
-      let data = this.userEnrollCourse?.filter(item => this.yourPlansCourseIdentifier?.includes(item.identifier))
-      if (this.configData) {
-        const completed = data?.filter(item => item.completionPercentage === 100)
-        const incomplete = data?.filter(item => item.completionPercentage !== 100)
-        this.configData?.forEach((element: any) => {
-          if (element.playlistConfigId === 'CONTINUE_LEARNING') {
-            element.data = incomplete
-            element.displayData = element?.data?.slice(0, element.limit)
-          } else if (element.playlistConfigId === 'YOUR_PLANS_PLAYLIST') {
-            element.data = this.coursesForYou.filter(item =>
-              !this.userEnrollCourse.some(bItem => bItem.identifier === item.identifier)
-            )
-            element.displayData = element?.data?.slice(0, element.limit)
-          } else if (element.playlistConfigId === 'CNE_COURSE_PLAYLIST') {
-            element.data = this.cneCourse
-            element.displayData = element?.data?.slice(0, element.limit)
-          } else if (element.playlistConfigId === 'TOP_COURSE_PLAYLIST') {
-            element.data = !this.isEkshamata ? this.topCertifiedCourse : this.coursesForEK
-            element.displayData = element?.data?.slice(0, element.limit)
-          } else if (element.playlistConfigId === 'COMPLETED') {
-            element.data = completed
-            element.displayData = element?.data?.slice(0, element.limit)
-          }
-        })
-      }
+      this.updateCourseData()
     })
   }
 
+  updateCourseData() {
+    let data = this.userEnrollCourse?.filter(item => this.yourPlansCourseIdentifier?.includes(item.identifier))
+    if (this.configData) {
+      const completed = data?.filter(item => item.completionPercentage === 100)
+      const incomplete = data?.filter(item => item.completionPercentage !== 100)
+      this.configData?.forEach((element: any) => {
+        if (element.playlistConfigId === 'CONTINUE_LEARNING') {
+          element.data = incomplete
+          element.displayData = element?.data?.slice(0, element.limit)
+        } else if (element.playlistConfigId === 'YOUR_PLANS_PLAYLIST') {
+          element.data = this.coursesForYou.filter(item =>
+            !this.userEnrollCourse.some(bItem => bItem.identifier === item.identifier)
+          )
+          element.displayData = element?.data?.slice(0, element.limit)
+        } else if (element.playlistConfigId === 'CNE_COURSE_PLAYLIST') {
+          element.data = this.cneCourse
+          element.displayData = element?.data?.slice(0, element.limit)
+        } else if (element.playlistConfigId === 'TOP_COURSE_PLAYLIST') {
+          element.data = !this.isEkshamata ? this.topCertifiedCourse : this.coursesForEK
+          element.displayData = element?.data?.slice(0, element.limit)
+        } else if (element.playlistConfigId === 'COMPLETED') {
+          element.data = completed
+          element.displayData = element?.data?.slice(0, element.limit)
+        }
+      })
+    }
+  }
 
   // For opening Course Page
   raiseTelemetry(contentIdentifier: any) {
