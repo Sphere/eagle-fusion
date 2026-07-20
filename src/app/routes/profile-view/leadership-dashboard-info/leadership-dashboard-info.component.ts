dabc1bb6f9427ee6a960358650ef43a5ee5c74ac
vpPavithra <pavithra.prakash@tarento.com>
2026-07-20 14:43:25 +0530

    Soanr fixes - removed empty functions

--- src/app/routes/profile-view/leadership-dashboard-info/leadership-dashboard-info.component.ts
+++ src/app/routes/profile-view/leadership-dashboard-info/leadership-dashboard-info.component.ts
@@ -1,20 +1,18 @@
-import { Component, Inject, OnInit } from '@angular/core'
+import { Component, Inject } from '@angular/core'
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog'
 
 @Component({
-    standalone: false,
-    selector: 'app-leadership-dashboard-info',
-    templateUrl: './leadership-dashboard-info.component.html',
-    styleUrls: ['./leadership-dashboard-info.component.scss'],
-    
+  standalone: false,
+  selector: 'app-leadership-dashboard-info',
+  templateUrl: './leadership-dashboard-info.component.html',
+  styleUrls: ['./leadership-dashboard-info.component.scss'],
+
 })
-export class LeadershipDashboardInfoComponent implements OnInit {
+export class LeadershipDashboardInfoComponent {
   constructor(
     @Inject(MAT_DIALOG_DATA) public data: any,
     public dialogRef: MatDialogRef<LeadershipDashboardInfoComponent>,
   ) { }
-
-  ngOnInit() { }
 
   close() {
     this.dialogRef.close()
